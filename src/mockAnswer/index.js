9bd144b4abce9ecc3441215f3fe1e0ab17b03561
nsetya <novitasetya.u@gmail.com>
2023-03-30 15:45:56 +0700

    move into mock file

---
+++ src/mockAnswer/index.js
@@ -0,0 +1,134 @@
+export const mockAnswerP2 = [
+  {
+    id: "P2.1", // acts as primary key, should be unique and non-empty string
+    label: "≤ 15 menit",
+    value: "≤ 15 menit",
+    size: 20,
+  },
+  {
+    id: "P2.2",
+    label: "16-30 menit",
+    value: "16-30 menit",
+    size: 20,
+  },
+  {
+    id: "P2.3",
+    label: "31-60 menit",
+    value: "31-60 menit",
+    size: 20,
+  },
+  {
+    id: "P3.4",
+    label: "> 60 menit",
+    value: "> 60 menit",
+    size: 20,
+  },
+];
+
+export const mockAnswerP4 = [
+  {
+    id: "P4.1", // acts as primary key, should be unique and non-empty string
+    label: "> 7 jam",
+    value: "> 7 jam",
+    size: 20,
+  },
+  {
+    id: "P4.2",
+    label: "6-7 jam",
+    value: "6-7 jam",
+    size: 20,
+  },
+  {
+    id: "P4.3",
+    label: "5-6 jam",
+    value: "5-6 jam",
+    size: 20,
+  },
+  {
+    id: "P4.4",
+    label: "<5 jam",
+    value: "<5 jam",
+    size: 20,
+  },
+];
+
+export const mockAnswerP5 = [
+  {
+    id: "P5.1", // acts as primary key, should be unique and non-empty string
+    label: "Tidak pernah dalam satu bulan terakhir",
+    value: "Tidak pernah dalam satu bulan terakhir",
+    size: 20,
+  },
+  {
+    id: "P5.2",
+    label: "1x Seminggu",
+    value: "1x Seminggu",
+    size: 20,
+  },
+  {
+    id: "P5.3",
+    label: "2x Seminggu",
+    value: "2x Seminggu",
+    size: 20,
+  },
+  {
+    id: "P5.4",
+    label: ">= 3x seminggu",
+    value: ">= 3x seminggu",
+    size: 20,
+  },
+];
+
+export const mockAnswerP8 = [
+  {
+    id: "P8.1", // acts as primary key, should be unique and non-empty string
+    label: "Tidak antusias",
+    value: "Tidak antusias",
+    size: 20,
+  },
+  {
+    id: "P8.2",
+    label: "Kecil",
+    value: "Kecil",
+    size: 20,
+  },
+  {
+    id: "P8.3",
+    label: "Sedang",
+    value: "Sedang",
+    size: 20,
+  },
+  {
+    id: "P8.4",
+    label: "Besar",
+    value: "Besar",
+    size: 20,
+  },
+];
+
+export const mockAnswerP9 = [
+  {
+    id: "P9.1", // acts as primary key, should be unique and non-empty string
+    label: "Sangat baik",
+    value: "Sangat baik",
+    size: 20,
+  },
+  {
+    id: "P9.2",
+    label: "Cukup baik",
+    value: "Cukup baik",
+    size: 20,
+  },
+  {
+    id: "P9.3",
+    label: "Cukup buruk",
+    value: "Cukup buruk",
+    size: 20,
+  },
+  {
+    id: "P9.4",
+    label: "Sangat buruk",
+    value: "Sangat buruk",
+    size: 20,
+  },
+];
--- src/screen/Kuesioner.js
+++ src/screen/Kuesioner.js
@@ -1,145 +1,23 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, ScrollView } from "react-native";
 import RadioGroup from "react-native-radio-buttons-group";
+import {
+  mockAnswerP2,
+  mockAnswerP4,
+  mockAnswerP5,
+  mockAnswerP8,
+  mockAnswerP9,
+} from "../mockAnswer/index.js";
 import Style from "../styles/kuesioner-style.js";
 
 export default function Kuesioner({ navigation }) {
   const [sleepTime, onChangeSleepTime] = React.useState("");
   const [awakeTime, onChangeAwakeTime] = React.useState("");
-  const [answerP2, setAnswerP2] = useState([
-    {
-      id: "P2.1", // acts as primary key, should be unique and non-empty string
-      label: "≤ 15 menit",
-      value: "≤ 15 menit",
-      size: 20,
-    },
-    {
-      id: "P2.2",
-      label: "16-30 menit",
-      value: "16-30 menit",
-      size: 20,
-    },
-    {
-      id: "P2.3",
-      label: "31-60 menit",
-      value: "31-60 menit",
-      size: 20,
-    },
-    {
-      id: "P3.4",
-      label: "> 60 menit",
-      value: "> 60 menit",
-      size: 20,
-    },
-  ]);
-
-  const [answerP4, setAnswerP4] = useState([
-    {
-      id: "P4.1", // acts as primary key, should be unique and non-empty string
-      label: "> 7 jam",
-      value: "> 7 jam",
-      size: 20,
-    },
-    {
-      id: "P4.2",
-      label: "6-7 jam",
-      value: "6-7 jam",
-      size: 20,
-    },
-    {
-      id: "P4.3",
-      label: "5-6 jam",
-      value: "5-6 jam",
-      size: 20,
-    },
-    {
-      id: "P4.4",
-      label: "<5 jam",
-      value: "<5 jam",
-      size: 20,
-    },
-  ]);
-
-  const [answerP5, setAnswerP5] = useState([
-    {
-      id: "P5.1", // acts as primary key, should be unique and non-empty string
-      label: "Tidak pernah dalam satu bulan terakhir",
-      value: "Tidak pernah dalam satu bulan terakhir",
-      size: 20,
-    },
-    {
-      id: "P5.2",
-      label: "1x Seminggu",
-      value: "1x Seminggu",
-      size: 20,
-    },
-    {
-      id: "P5.3",
-      label: "2x Seminggu",
-      value: "2x Seminggu",
-      size: 20,
-    },
-    {
-      id: "P5.4",
-      label: ">= 3x seminggu",
-      value: ">= 3x seminggu",
-      size: 20,
-    },
-  ]);
-
-  const [answerP8, setAnswerP8] = useState([
-    {
-      id: "P8.1", // acts as primary key, should be unique and non-empty string
-      label: "Tidak antusias",
-      value: "Tidak antusias",
-      size: 20,
-    },
-    {
-      id: "P8.2",
-      label: "Kecil",
-      value: "Kecil",
-      size: 20,
-    },
-    {
-      id: "P8.3",
-      label: "Sedang",
-      value: "Sedang",
-      size: 20,
-    },
-    {
-      id: "P8.4",
-      label: "Besar",
-      value: "Besar",
-      size: 20,
-    },
-  ]);
-
-  const [answerP9, setAnswerP9] = useState([
-    {
-      id: "P9.1", // acts as primary key, should be unique and non-empty string
-      label: "Sangat baik",
-      value: "Sangat baik",
-      size: 20,
-    },
-    {
-      id: "P9.2",
-      label: "Cukup baik",
-      value: "Cukup baik",
-      size: 20,
-    },
-    {
-      id: "P9.3",
-      label: "Cukup buruk",
-      value: "Cukup buruk",
-      size: 20,
-    },
-    {
-      id: "P9.4",
-      label: "Sangat buruk",
-      value: "Sangat buruk",
-      size: 20,
-    },
-  ]);
+  const [answerP2, setAnswerP2] = useState(mockAnswerP2);
+  const [answerP4, setAnswerP4] = useState(mockAnswerP4);
+  const [answerP5, setAnswerP5] = useState(mockAnswerP5);
+  const [answerP8, setAnswerP8] = useState(mockAnswerP8);
+  const [answerP9, setAnswerP9] = useState(mockAnswerP9);
 
   return (
     <ScrollView>
@@ -155,7 +33,8 @@ export default function Kuesioner({ navigation }) {
           keyboardType="numeric"
         />
         <Text style={Style.pertanyaan}>
-          2. Berapa lama (dalam menit) yang anda perlukan untuk dapat mulai tertidur setiap malam?
+          2. Berapa lama (dalam menit) yang anda perlukan untuk dapat mulai
+          tertidur setiap malam?
         </Text>
         <RadioGroup
           containerStyle={Style.radioButton}
