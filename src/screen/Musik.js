f6aeb7b88e06eef835cb95e7dd3bd40ccc62a90e
nsetya <novitasetya.u@gmail.com>
2023-03-31 17:16:26 +0700

    add initial music code

--- src/screen/Musik.js
+++ src/screen/Musik.js
@@ -1,186 +1,80 @@
-import React from "react";
-import { View, Text, Image, ProgressBarAndroid } from "react-native";
-import * as Progress from "react-native-progress";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { Audio } from "expo-av";
 
 function Musik({ navigation }) {
+  const [sound, setSound] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      // require("../../assets/mp3/Camp_Fire_Sound.mp3")
+      require("../../assets/Alarm.mp3")
+    );
+    setSound(sound);
+    setIsPlaying(true);
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  async function pauseSound() {
+    if (sound !== null) {
+      await sound.pauseAsync();
+      setIsPlaying(false);
+    }
+  }
+
   return (
     <View>
-      <Text style={{ marginLeft: 27, fontSize: 24, marginTop: 27 }}>
-        Musik Relaksasi
-      </Text>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 71 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 71 }}>Replenish</Text>
-        <Text style={{ marginLeft: 150, marginTop: 71 }}>09.42</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 71 }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 21 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 21 }}>Timing</Text>
-        <Text style={{ marginLeft: 169, marginTop: 21 }}>08.45</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 21 }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 21 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 21 }}>Sanctuary</Text>
-        <Text style={{ marginLeft: 149, marginTop: 21 }}>10.42</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 21 }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 21 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 21 }}>Motion</Text>
-        <Text style={{ marginLeft: 170, marginTop: 21 }}>08.33</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 21 }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 21 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 21 }}>Floating</Text>
-        <Text style={{ marginLeft: 163, marginTop: 21 }}>09.21</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 21 }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 21 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 21 }}>Earth</Text>
-        <Text style={{ marginLeft: 182, marginTop: 21 }}>10.25</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 21 }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 21 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 21 }}>Blossom</Text>
-        <Text style={{ marginLeft: 160, marginTop: 21 }}>09.42</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 21 }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/plumpy/24/null/play-button-circled--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 27, marginTop: 21 }}
-        />
-        <Text style={{ marginLeft: 20, marginTop: 21 }}>Blessing</Text>
-        <Text style={{ marginLeft: 162, marginTop: 21 }}>08.40</Text>
-        <Image
-          source={{
-            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
-          }}
-          style={{ width: 24, height: 24, marginLeft: 20, marginTop: 21 }}
-        />
-      </View>
-      <View
-        style={{
-          backgroundColor: "#82B8AD80",
-          width: 367,
-          height: 120,
-          marginLeft: 20,
-          marginTop: 162,
-          borderRadius: 10,
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "row",
-        }}
-      >
-        <Image
-          source={{
-            uri: "https://img.icons8.com/flat-round/64/null/play--v1.png",
-          }}
-          style={{ width: 52, height: 52 }}
-        />
-        <View
-          style={{
-            marginLeft: 16,
-          }}
+      <Text style={styles.text}>Musik Relaksasi</Text>
+      <View style={{ flexDirection: "row", padding: 10 }}>
+        <TouchableOpacity
+          onPress={() => (isPlaying ? pauseSound() : playSound())}
         >
-          <Text>Replenish</Text>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Progress.Bar progress={0.3} width={160} color={"#518578"} />
-            <Text style={{ marginLeft: 30 }}>03.59</Text>
+          {isPlaying ? (
             <Image
               source={{
-                uri: "https://img.icons8.com/ios/50/null/like--v1.png",
+                uri: "https://img.icons8.com/stickers/100/null/pause-squared.png",
               }}
-              style={{ width: 24, height: 24, marginLeft: 10 }}
+              style={{ width: 24, height: 24, marginLeft: 27, marginTop: 71 }}
             />
-          </View>
-        </View>
+          ) : (
+            <Image
+              source={{
+                uri: "https://img.icons8.com/plasticine/100/null/play.png",
+              }}
+              style={{ width: 24, height: 24, marginLeft: 27, marginTop: 71 }}
+            />
+          )}
+        </TouchableOpacity>
+        <Text style={{ marginLeft: 20, marginTop: 71 }}>Replenish</Text>
+        <Text style={{ marginLeft: 120, marginTop: 71 }}>09.42</Text>
+        <Image
+          source={{
+            uri: "https://img.icons8.com/ios/50/null/like--v1.png",
+          }}
+          style={{ width: 24, height: 24, marginLeft: 10, marginTop: 71 }}
+        />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    marginLeft: 27,
+    fontSize: 24,
+    marginTop: 27,
+    marginBottom: 26,
+  },
+});
 
 export default Musik;
